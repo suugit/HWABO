f042f537428ee002ceae658c4fc75e8951635ed7
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2020-09-08 15:31:57 +0900

    복구 ㅠ

--- src/main/java/com/beet/HWABO/member/model/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.beet.HWABO.member.model.service;
-
-import com.beet.HWABO.member.model.vo.Member;
-import com.beet.HWABO.test.model.vo.CryptoUser;
-
-public interface MemberService {
-	int insertUser(Member member);
-	Member selectLogin(Member member);
-}
