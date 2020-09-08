9d5dc891e796938759e8c39606d7a52ec6b2e442
kyu-baek <69143394+kyu-baek@users.noreply.github.com>
2020-09-08 21:28:43 +0900

    패키지

---
+++ src/main/java/com/beet/HWABO/bpost/model/service/BpostServiceImpl.java
@@ -0,0 +1,19 @@
+package com.beet.HWABO.bpost.model.service;
+
+import org.springframework.stereotype.Service;
+
+import com.beet.HWABO.bpost.model.dao.BpostDao;
+import com.beet.HWABO.bpost.model.vo.Bpost;
+
+@Service("bpostService")
+public class BpostServiceImpl implements BpostService{
+
+	private BpostDao bpostDao;
+	
+	@Override
+	public int insertBpost(Bpost bpost) {
+	
+		return bpostDao.insertBpost(bpost);
+	}
+
+}
