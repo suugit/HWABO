f3b523b05b26ce4fefe29a74eaabb05e827b1e76
hckim21c11 <hckim21c11@naver.com>
2020-09-09 20:10:12 +0900

    할일 작업하기

--- src/main/java/com/beet/HWABO/dopost/model/service/DopostService.java
+++ src/main/java/com/beet/HWABO/dopost/model/service/DopostService.java
@@ -1,5 +1,11 @@
 package com.beet.HWABO.dopost.model.service;
 
-public interface DopostService {
+import com.beet.HWABO.dopost.model.vo.Dopost;
 
+public interface DopostService {
+	int insertOrigindopost(Dopost dopost);
+	 int deletedopost(Dopost dopost);
+	 int updateOrigin( Dopost dopost);
+	 int updateAddReadCount(int dopost_num);
+	 Dopost selectdopost(int dopost_num);
 }
--- src/main/java/com/beet/HWABO/dopost/model/service/DopostServiceImpl.java
+++ src/main/java/com/beet/HWABO/dopost/model/service/DopostServiceImpl.java
@@ -1,5 +1,46 @@
 package com.beet.HWABO.dopost.model.service;
 
-public class DopostServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.beet.HWABO.dopost.model.vo.Dopost;
+
+@Service("dopostService")
+public class DopostServiceImpl implements DopostService {
+	
+	@Autowired
+	private DopostDao dopostDao;
+
+	@Override
+	public int insertOrigindopost(Dopost dopost) {
+		return dopostDao.insertOrigindopost(dopost);
+	}
+
+	@Override
+	public int deletedopost(Dopost dopost) {
+		return dopostDao.deletedopost(dopost);
+	}
+
+	@Override
+	public int updateOrigindopost(Dopost dopost) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateAddReadCountdopost(int dopost_num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Dopost selectdopost(int dopost_num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	
+	
 }
