bf01e981a6e2eca0b7d90f7bf00e82d56e658f19
suugit <69067257+suugit@users.noreply.github.com>
2020-09-09 17:12:07 +0900

    이메일로 회원가입 인증링크
    유효성 검사 아직 안함

--- src/main/java/com/beet/HWABO/member/model/service/MemberService.java
+++ src/main/java/com/beet/HWABO/member/model/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.beet.HWABO.member.model.service;
 
+import java.util.Map;
+
 import com.beet.HWABO.member.model.vo.Member;
 import com.beet.HWABO.test.model.vo.CryptoUser;
 
 public interface MemberService {
 	int insertUser(Member member);
 	Member selectLogin(Member member);
+	int updateAuth(Member member);
+	int updateUst(Member member);
 }
