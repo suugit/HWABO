6bee5111c7bfb6a017a483bf344ec077f3f823ee
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2020-09-10 16:01:04 +0900

    또 삭제

--- src/main/java/com/beet/HWABO/test/model/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.beet.HWABO.test.model.service;
-
-import com.beet.HWABO.test.model.vo.CryptoUser;
-
-public interface TestService {
-	int insertUser(CryptoUser user);
-	CryptoUser selectLogin(CryptoUser user);
-}
