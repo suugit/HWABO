f314245b98e1866701d0e9fcd1cd0320b491359b
suugit <69067257+suugit@users.noreply.github.com>
2020-09-11 18:33:22 +0900

    게시글 다중파일 맵핑까지완료

--- src/main/java/com/beet/HWABO/cpost/model/service/CpostService.java
+++ src/main/java/com/beet/HWABO/cpost/model/service/CpostService.java
@@ -1,7 +1,10 @@
 package com.beet.HWABO.cpost.model.service;
 
+import com.beet.HWABO.cpost.model.vo.AddOn;
 import com.beet.HWABO.cpost.model.vo.Cpost;
 
 public interface CpostService {
 	int insertCpost(Cpost cpost);
+
+	int insertAddon(AddOn addon);
 }
