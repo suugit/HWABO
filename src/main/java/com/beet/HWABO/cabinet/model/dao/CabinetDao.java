0b5103b235c0388cad5d509a3c432bbf70934b8e
kyu-baek <69143394+kyu-baek@users.noreply.github.com>
2020-09-12 22:55:47 +0900

    아이고

---
+++ src/main/java/com/beet/HWABO/cabinet/model/dao/CabinetDao.java
@@ -0,0 +1,26 @@
+package com.beet.HWABO.cabinet.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.beet.HWABO.cabinet.model.vo.Cabinet;
+
+@Repository("cabinetDao")
+public class CabinetDao {
+
+	private static final Logger logger = LoggerFactory.getLogger(CabinetDao.class);
+	
+	@Autowired
+	private SqlSessionTemplate session;
+	
+	public CabinetDao() {}
+	
+	public int insertCabinet(Cabinet cabinet) {
+		logger.info("cabinet dao :"+ cabinet);
+		return session.insert("cabinetMappper.insertCabinet", cabinet);
+	}
+
+}
---
+++ src/main/java/com/beet/HWABO/cabinet/model/service/CabinetService.java
@@ -0,0 +1,9 @@
+package com.beet.HWABO.cabinet.model.service;
+
+import com.beet.HWABO.cabinet.model.vo.Cabinet;
+
+public interface CabinetService {
+
+	int insertCabinet(Cabinet cabinet);
+	
+}
