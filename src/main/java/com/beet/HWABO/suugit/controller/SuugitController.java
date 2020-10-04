f186ebbc0ed0e8caa20b3bb610a6b6e081e24a50
suugit <69067257+suugit@users.noreply.github.com>
2020-10-04 20:42:39 +0900

    slf4j 버전수정
    1.6.6 -> 1.7.25

--- src/main/java/com/beet/HWABO/suugit/controller/SuugitController.java
+++ src/main/java/com/beet/HWABO/suugit/controller/SuugitController.java
@@ -21,7 +21,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.apache.catalina.Session;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
